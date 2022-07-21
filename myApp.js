5bc6ca3d830fb6effa45c5172964b8e8193a2aca
Abraomukas <abraomukas@gmail.com>
2022-07-21 12:28:57 +0400

    feat: added findById()

--- myApp.js
+++ myApp.js
@@ -55,11 +55,17 @@ const findPeopleByName = (personName, done) => {
 };
 
 const findOneByFood = (food, done) => {
-	done(null /*, data*/);
+	Person.findOne({ favoriteFoods: food }, (err, data) => {
+		if (err) return console.error(err);
+		done(null, data);
+	});
 };
 
 const findPersonById = (personId, done) => {
-	done(null /*, data*/);
+	Person.findById({ _id: personId }, (err, data) => {
+		if (err) return console.error(err);
+		done(null, data);
+	});
 };
 
 const findEditThenSave = (personId, done) => {
